f2b6a09113f72f2c06d8fcf573cc252183dfd2d6
Samuel Steele <steelesam72@gmail.com>
2015-05-22 12:00:54 -0400

    Initial Commit

---
+++ SpeechPrep/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SpeechPrep
+//
+//  Created by Cryptoc1 on 5/22/15.
+//  Copyright (c) 2015 Cryptocosm Developers. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
